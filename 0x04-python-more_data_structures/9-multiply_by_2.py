983ca4742777298c9a5fa309820195c7618945d9
Ahmed <ahmedanwarkhaliel@gmail.com>
2024-01-11 07:35:17 -0800

    789101112

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return(dict(map(lambda item: (item[0], item[1] * 2)), a_dictionary.items()))
+    return(dict(map(lambda item: (item[0],
+            item[1] * 2)), a_dictionary.items()))
