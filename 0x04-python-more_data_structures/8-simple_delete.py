1c2b2677a08cb8c4a2d2c64230221ace3648af7c
Ahmed <ahmedanwarkhaliel@gmail.com>
2024-01-11 07:33:58 -0800

    789101112

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,5 @@ def simple_delete(a_dictionary, key=""):
     new = list(a_dictionary)
     for i in new:
         if i == key:
-            del(a_dictionary[i])
+            del a_dictionary[i]
     return a_dictionary
